9fc12bad47f1c2e26ad6094fd75a0e01540ed857
J To <donthaveapps@gmail.com>
2020-09-14 14:00:34 +0800

    Rearranged as function

--- hk80toWGS84_example.py
+++ hk80toWGS84_example.py
@@ -1,9 +1,10 @@
 import pyproj
 
-inProj = pyproj.Proj("ESRI:102140")  # This is the HK1980 Grid
-outProj = pyproj.Proj("epsg:4326")
+def hk80toWGS(x_in, y_in):
+    inProj = pyproj.Proj("ESRI:102140")  # This is the HK1980 Grid
+    outProj = pyproj.Proj("epsg:4326")  # This is WGS 84
+    latitude, longitude = pyproj.transform(inProj, outProj, x_in, y_in)
+    return f"{latitude}.{longitude}"
 
-x_in, y_in = 840734, 844917.6
-latitude, longitude = pyproj.transform(inProj, outProj, x_in, y_in)
-
-print(latitude, longitude)
+# x_in, y_in = 840734, 844917.6
+# print(hk80toWGS(x_in, y_in))
