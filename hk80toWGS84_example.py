818bd95fdac6605cbb5c5c4be33618c01ab6c4bf
J To <donthaveapps@gmail.com>
2020-09-14 14:19:01 +0800

    Add function for tabpy returning pandas dataframe

--- hk80toWGS84_example.py
+++ hk80toWGS84_example.py
@@ -1,10 +1,21 @@
 import pyproj
+import pandas as pd
 
 def hk80toWGS(x_in, y_in):
     inProj = pyproj.Proj("ESRI:102140")  # This is the HK1980 Grid
     outProj = pyproj.Proj("epsg:4326")  # This is WGS 84
     latitude, longitude = pyproj.transform(inProj, outProj, x_in, y_in)
-    return f"{latitude},{longitude}"
+    return (latitude,longitude)
 
 # x_in, y_in = 840734, 844917.6
 # print(hk80toWGS(x_in, y_in))
+
+def tabpyHK80toWGS(input):
+    inProj = pyproj.Proj("ESRI:102140")  # This is the HK1980 Grid
+    outProj = pyproj.Proj("epsg:4326")  # This is WGS 84
+    return pd.Dataframe({
+        "Easting": input["Easting"],
+        "Northing": input["Northing"],
+        "Latitude": hk80toWGS(input["Easting"], input["Northing"])[0],
+        "Latitude": hk80toWGS(input["Easting"], input["Northing"])[1]
+    })
