1ca86c04502f890ab4ed0ec6cb1e01ce388528fa
J To <donthaveapps@gmail.com>
2020-09-14 14:01:12 +0800

    Corrected typo

--- hk80toWGS84_example.py
+++ hk80toWGS84_example.py
@@ -4,7 +4,7 @@ def hk80toWGS(x_in, y_in):
     inProj = pyproj.Proj("ESRI:102140")  # This is the HK1980 Grid
     outProj = pyproj.Proj("epsg:4326")  # This is WGS 84
     latitude, longitude = pyproj.transform(inProj, outProj, x_in, y_in)
-    return f"{latitude}.{longitude}"
+    return f"{latitude},{longitude}"
 
 # x_in, y_in = 840734, 844917.6
 # print(hk80toWGS(x_in, y_in))
